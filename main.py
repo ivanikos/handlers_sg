6b1b2ab3d23e5c268403606144ded12e80c6b483
ignatenkoia <vanikos@yandex.ru>
2022-11-28 12:52:52 +0900

    28.11.22 handler 2.0 в работе интерфейс

---
+++ main.py
@@ -0,0 +1,15 @@
+import eel
+
+
+
+@eel.expose
+# defining the function for addition of two numbers
+def add(data_1, data_2):
+    int1 = int(data_1)
+    int2 = int(data_2)
+    output = int1 + int2
+    return output
+
+if __name__ == '__main__':
+    eel.init('front')
+    eel.start('index.html', mode="chrome", size=(600, 400))
